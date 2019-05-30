110d40b4adacbec066bceb0b83591d816b32cd96
Scott Wren <scott.wren@nrtsoutheast.com>
2019-05-30 11:54:31 -0400

    Add name-detail logic

--- src/app/name-add/name-add.component.ts
+++ src/app/name-add/name-add.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-name-add',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NameAddComponent implements OnInit {
 
-  constructor() { }
+  nameForm: FormGroup;
+  name: string = '';
+  address: string = '';
+  isLoading = true;
+
+  constructor(private router: Router, private api: ApiService, private formBuilder: FormBuilder) { 
+
+  }
 
   ngOnInit() {
+    this.nameForm = this.formBuilder.group({
+      'name' : [null, Validators.required]
+    });
+  }
+
+  onSubmit(form:NgForm) {
+    this.isLoading = true;
+    this.api.addName(form)
+    .subscribe(res => {
+      let id = res['_id'];
+      this.isLoading = false;
+      this.router.navigate(['/name-detail', id])
+    }, (err) => {
+      console.log(err);
+      this.isLoading = false;
+    })
   }
 
 }
